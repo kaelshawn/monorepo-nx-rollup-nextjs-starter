6e8662444bc18369893964d7b038122e87c2abe8
Kael Shawn <kaelshawn@outlook.com>
2023-05-28 22:07:52 +0800

    build test

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,6 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+
 export default function RootLayout({
   children,
 }: {
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
 import { Button, Header } from "ui";
+import { Button as PrimeButton } from "primereact/button";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <PrimeButton onClick={()=>{
+        
+      }}>123123</PrimeButton>
     </>
   );
 }
