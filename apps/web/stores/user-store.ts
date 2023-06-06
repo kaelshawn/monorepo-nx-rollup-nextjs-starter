84544003dccd26710437f9f99f11a8754c914f0f
Kael Shawn <kaelshawn@outlook.com>
2023-06-06 23:44:36 +0800

    add dev tools

--- apps/web/stores/user-store.ts
+++ apps/web/stores/user-store.ts
@@ -1,5 +1,8 @@
 import { UserModel } from "models";
 import { proxy } from "valtio";
+import { devtools } from "valtio/utils";
 
 const user = new UserModel();
 export const userStore = proxy<UserModel>(user);
+
+devtools(userStore, { name: "user", enabled: true });
