3d068af86388caee594afcd2bda34a4071a36a34
Kael Shawn <kaelshawn@outlook.com>
2023-06-11 18:21:24 +0800

    nx generate @nx/react:component logo --project=ui --export

---
+++ packages/ui/lib/logo/logo.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Logo from './logo';
+
+describe('Logo', () => {
+  
+  it('should render successfully', () => {
+    const { baseElement } = render(<Logo />);
+    expect(baseElement).toBeTruthy();
+  });
+  
+});
---
+++ packages/ui/lib/logo/logo.tsx
@@ -0,0 +1,28 @@
+
+
+
+
+
+  import styles from './logo.module.scss';
+  
+
+/* eslint-disable-next-line */
+export interface LogoProps {
+}
+
+
+
+export function Logo(props: LogoProps) {
+  return (
+    <div className={styles['container']}>
+      
+      <h1>Welcome to Logo!</h1>
+      
+    </div>
+  );
+};
+
+
+export default Logo;
+
+
