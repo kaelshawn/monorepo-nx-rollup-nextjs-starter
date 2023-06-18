ca96e0de1c83d68bf37f14a3a228c3abe26231ce
Kael Shawn <kaelshawn@outlook.com>
2023-06-18 08:18:26 +0800

    next Script not effect for native js file

--- apps/web/pages/test-pdf/index.tsx
+++ apps/web/pages/test-pdf/index.tsx
@@ -1,6 +1,4 @@
 import dynamic from "next/dynamic";
-import Script from "next/script";
-import pdfjs from "../../pdf";
 
 const PDFViewer = dynamic(() => import("../../components/pdf-reader"), {
   ssr: false,
@@ -10,12 +8,7 @@ function PDF() {
   return (
     <>
       <div>123123</div>
-      <Script
-        src={pdfjs}
-        onReady={() => {
-          debugger;
-        }}
-      ></Script>
+
       <PDFViewer pdfUrl="/sample.pdf" />
     </>
   );
